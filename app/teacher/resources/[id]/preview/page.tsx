0588a2898452c454f526e37111c4fbfd194fafbb
Georgina Mudditt <g.k.mudditt@gmail.com>
2025-12-31 07:14:00 +0100

    Convert resource preview page from Prisma to Supabase REST API

--- app/teacher/resources/[id]/preview/page.tsx
+++ app/teacher/resources/[id]/preview/page.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
 import { redirect } from 'next/navigation'
-import { prisma } from '@/lib/prisma'
+import { supabaseServer } from '@/lib/supabase'
 import Navbar from '@/components/Navbar'
 import ResourcePreview from '@/components/ResourcePreview'
 import Link from 'next/link'
